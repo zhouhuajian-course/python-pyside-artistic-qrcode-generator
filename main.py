b19a1308a2e4cff92eebe5df9ba083d0bd948c36
zhouhuajian <zhouhuajian@email.com>
2022-07-06 10:02:10 +0800

    主界面设计

--- main.py
+++ main.py
@@ -4,7 +4,8 @@
 @author  : zhouhuajian
 @version : v1.0
 """
-from PySide6.QtWidgets import QWidget, QApplication
+from PySide6.QtGui import QMouseEvent, Qt
+from PySide6.QtWidgets import QWidget, QApplication, QFileDialog
 
 from main_window_ui import Ui_MainWindow
 
@@ -18,6 +19,20 @@ class ArtisticQrcodeGenerator(QWidget):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
+        self.ui.backgroundImageLabel.mousePressEvent = self.changeBackgroundImage
+
+    def changeBackgroundImage(self, mouseEvent: QMouseEvent):
+        """修改背景图片"""
+        if mouseEvent.button() != Qt.MouseButton.LeftButton:
+            return
+        r = QFileDialog.getOpenFileName(parent=self,
+                                        caption="选择背景图片",
+                                        filter="图片 (*.jpg *.png *.gif)")
+        # print(r)
+        imagePath = r[0]
+        if not imagePath:
+            return
+        print("背景图片修改为" + imagePath)
 
 if __name__ == '__main__':
     # 应用
